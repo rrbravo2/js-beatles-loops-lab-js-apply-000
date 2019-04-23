681f68afe4a0175ed8d2d8f9b74dfa994c0683db
Ryan Bravo <rrbravo2@gmail.com>
2019-04-23 09:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []; 
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`); 
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var shoutedFacts = []
+  
+  let i = 0 
+  while(i > )
+}
